1fdfe5013103292d7897ef638bd9d9868023b4a3
czantoine <ac11f@free.fr>
2022-02-02 13:20:39 +0100

    Update master_deployement.sh

--- auto/master_deployement.sh
+++ auto/master_deployement.sh
@@ -3,9 +3,9 @@
 sudo swapoff -a
 sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-sudo name=$(cat /etc/hostname)
-PUBLIC_IP_ADDRESS=`hostname -I|cut -d" " -f 1`
-sudo echo "${PUBLIC_IP_ADDRESS}  $name" >> /etc/hosts
+#sudo name=$(cat /etc/hostname)
+#PUBLIC_IP_ADDRESS=`hostname -I|cut -d" " -f 1`
+#sudo echo "${PUBLIC_IP_ADDRESS}  $name" >> /etc/hosts
 sudo apt-get purge aufs-tools docker-ce docker-ce-cli containerd.io pigz cgroupfs-mount -y
 sudo apt-get purge kubeadm kubernetes-cni -y
 sudo rm -rf /etc/kubernetes
@@ -46,8 +46,8 @@ sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 echo "Installing Kubernetes..."
 sudo apt install kubeadm -y
-sudo kubeadm init --apiserver-advertise-address=192.168.56.30 --pod-network-cidr=$PUBLIC_IP_ADDRESS/16
-sudo sleep 10
+sudo kubeadm init --apiserver-advertise-address=192.168.56.30 --pod-network-cidr=10.244.0.0/16
+sudo sleep 10 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -56,8 +56,8 @@ echo "Installing Flannel..."
 export KUBECONFIG=$HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 echo "Kubernetes Installation finished..."
-echo "Waiting 30 seconds for the cluster running..."
-sudo sleep 30
+echo "Waiting 20 seconds for the cluster running..."
+sudo sleep 20
 
 echo "Testing Kubernetes namespaces... "
 kubectl get pods --all-namespaces
@@ -70,5 +70,19 @@ echo "Token deployed..."
 # Deployement k8s
 kubectl create namespace k8s-webapp
 echo "Namespaces k8s-webapp created..."
+
+wget https://raw.githubusercontent.com/czantoine/kubernetes-full-auto-deployement/main/secret.yaml
+wget https://raw.githubusercontent.com/czantoine/kubernetes-full-auto-deployement/main/mysql-service.yml
+wget https://raw.githubusercontent.com/czantoine/kubernetes-full-auto-deployement/main/mysql-deployment.yaml
+
+sudo kubectl apply -f secret.yaml
+sudo kubectl apply -f mysql-service.yaml
+sleep 5
+sudo kubectl apply -f mysql-deployment.yaml
+sleep 15
+
+
+
+
 
 echo "Finished !"
