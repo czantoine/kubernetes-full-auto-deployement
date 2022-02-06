60ecc639ff0b6b5f5ed0928da00b107f538090be
czantoine <ac11f@free.fr>
2022-02-06 18:06:54 +0100

    udpate docker compose

--- auto/master_deployement.sh
+++ auto/master_deployement.sh
@@ -21,6 +21,8 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Setup Daemon
 sudo mkdir -p /etc/systemd/system/docker.service.d
